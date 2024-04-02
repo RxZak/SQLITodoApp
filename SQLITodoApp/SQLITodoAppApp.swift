864a9b7af8fd93087a15680411c6c9016e8ff6bd
RxZak <is.zakariae@gmail.com>
2024-04-02 07:56:47 +0000

    Initial Commit

---
+++ SQLITodoApp/SQLITodoAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  SQLITodoAppApp.swift
+//  SQLITodoApp
+//
+//  Created by Zakariae Ismaili on 2/4/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SQLITodoAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
