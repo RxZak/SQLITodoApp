207d78ac9da915306c092573a9e58fd11e8e46c2
RxZak <is.zakariae@gmail.com>
2024-04-02 09:12:56 +0000

    Login Screen Design
    - Added xcassets color and image
    - Added InputField Reusable Component

---
+++ SQLITodoApp/Auth/View/LoginView.swift
@@ -0,0 +1,73 @@
+//
+//  LoginView.swift
+//  SQLITodoApp
+//
+//  Created by Zakariae Ismaili on 2/4/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("SQLI")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+                
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email",
+                              placeholder: "name@sqli.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $email,
+                              title: "Password",
+                              placeholder: "Enter your Password",
+                              isSecureField: true)
+
+                }
+                .padding(.horizontal)
+                .padding(.top, 20)
+                
+                Button{
+                    print("Logged in")
+                } label: {
+                    HStack {
+                        Text("Login")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color.accentColor)
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                NavigationLink{
+                    
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Register")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
